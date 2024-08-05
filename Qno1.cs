b117582bad98ac43c4ccda36772d237cdb797f11
manishalama123 <manishalama136@gmail.com>
2024-08-05 19:06:04 +0545

    Add project files.

---
+++ Qno1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace dotnet_labreport
+{
+    internal class Qno1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Welcome {userName}, to dotnet technology");
+            Console.ReadKey();
+        }
+    }
+}
+
