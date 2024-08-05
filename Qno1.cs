5457b50f816b03ecdf1478ee1e971a5f87346023
manishalama123 <manishalama136@gmail.com>
2024-08-05 19:12:44 +0545

    Qno2.cs

--- Qno1.cs
+++ Qno1.cs
@@ -5,13 +5,13 @@ namespace dotnet_labreport
 {
     internal class Qno1
     {
-        static void Main(string[] args)
+       /* static void Main(string[] args)
         {
             Console.Write("Please enter your name: ");
             string userName = Console.ReadLine();
             Console.WriteLine($"Welcome {userName}, to dotnet technology");
             Console.ReadKey();
-        }
+        }*/
     }
 }
 
---
+++ Qno2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Qno2
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Enter the first integer:");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second integer:");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the third integer:");
+        int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+       
+        int sum = firstNumber + secondNumber + thirdNumber;
+
+        // Display the result
+        Console.WriteLine("The sum of the three integers is: " + sum);
+        Console.ReadKey();
+    }
+}
